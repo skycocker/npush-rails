a0ea78dfb50e2875e6c61f8c66e4aa059d308648
Michał Siwek <mike21@aol.pl>
2013-03-03 23:00:32 +0100

    Extend toheroku generator

--- lib/generators/npush/toheroku_generator.rb
+++ lib/generators/npush/toheroku_generator.rb
@@ -2,12 +2,28 @@ module Npush
   class ToherokuGenerator < Rails::Generators::Base
     def clone_npush_repo
       @appname = Rails.application.class.parent_name
-      @reponame = @appname.to_s.downcase
+      @reponame = 'npush' + @appname.to_s.downcase
+      @npush_secret = SecureRandom.base64
+      @npush_server = 'http://' + @reponame + '.herokuapp.com/'
       
       in_root do
         git :clone => "git://github.com/skycocker/npush.git"
         inside "npush" do
-          system 'echo "'+@reponame+'"'
+          create_file 'config.json' do
+            "{\n"+
+            '  "npush_secret": "' + @npush_secret + '",' + "\n"+
+            '  "port": "80"' + "\n"+
+            '}'
+          end
+          #system 'heroku create ' + @reponame
+        end
+        inside "config/initializers" do
+          create_file 'npush.rb' do
+            "unless Rails.env.production?\n"+
+            "  ENV['npush_server'] = '" + @npush_server + "'\n"+
+            "  ENV['npush_secret'] = '" + @npush_secret + "'\n"+
+            "end\n"
+          end
         end
       end
     end
--- lib/npush-rails/version.rb
+++ lib/npush-rails/version.rb
@@ -1,3 +1,3 @@
 module Npush
-  VERSION = "0.0.34"
+  VERSION = "0.0.43"
 end
